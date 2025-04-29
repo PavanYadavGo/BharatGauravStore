d782fbdc4b6ffad96f2a08f329945a7b93feff28
PavanYadavGo <Pavan3253@outlook.com>
2025-04-29 19:26:13 +0530

    rvfsegg

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -23,18 +23,18 @@ const ImageSlider = () => {
   }, []);
 
   return (
-    <div className="relative w-full h-[500px] overflow-hidden">
+    <div className="relative w-full overflow-hidden">
       <div
         className="flex transition-transform duration-700 ease-in-out"
         style={{ transform: `translateX(-${currentIndex * 100}%)`, width: `${images.length * 100}%` }}
       >
         {images.map((image, index) => (
-          <div key={index} className="w-full flex-shrink-0 h-[500px] relative">
+          <div key={index} className="w-full flex-shrink-0 aspect-[16/6] relative">
             <Image
               src={image}
               alt={`Slider Image ${index + 1}`}
               fill
-              className="object-cover"
+              className="object-contain" // Or object-cover depending on how you want it cropped
               priority={index < 2}
             />
           </div>
